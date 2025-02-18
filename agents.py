802a77836723a85fc20d753abd057288d8076f0e
Bisma Shafiq <bismashafiq26@gmail.com>
2025-02-18 10:58:30 +0500

    agents

--- agents.py
+++ agents.py
@@ -1,4 +1,16 @@
 import os
 from dotenv import load_dotenv
-from autogen 
+from autogen  import AssistantAgent
+
 load_dotenv()
+
+class ResearchAgent():
+    def __init__(self):
+        pass
+
+    def summary_paper(self,paper_summary):
+        pass
+    def analyze_paper(self,summary):
+        pass
+
+    
